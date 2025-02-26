4e44ef4cb4133f981e2c5fbc653bf2857cf98d67
Victor Welander <victortwelander@gmail.com>
2025-02-26 18:19:40 +0100

    removed packages

--- app/page.tsx
+++ app/page.tsx
@@ -19,20 +19,3 @@ export default function Index() {
     </div>
   );
 }
-
-/* 
-    <div className="flex h-screen items-center justify-center">
-      <div className="flex flex-col items-center justify-center text-center">
-        <h1 className="text-gray-1200 max-w-4xl text-4xl/9 font-semibold tracking-tight text-balance sm:text-5xl md:text-6xl">
-          Interfaces.dev <span className="text-gray-900">for sale</span>
-        </h1>
-        <p className="mt-3 max-w-md">
-          If you are interested in buying the domain please write an email to{" "}
-          <Link href="mailto:contact@victorwelander.com">
-            contact@victorwelander.com
-          </Link>
-          or contact me via <Link href="https://x.com/victorwelander_">X</Link>
-        </p>
-      </div>
-    </div>
-*/
